8f58a5523ea88ec9e39cbd5e59f2e4404f252a24
Sai Surya <saisurya7860@gmail.com>
2025-01-05 18:27:22 +0530

    added href to index.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -36,7 +36,7 @@ products.forEach((product) =>{
                 <option value="7">7</option>
                 <option value="8">8</option>
                 <option value="9">9</option>
-                <option value="10">10</option>
+                <option value="10">10<option value="11">+</option></option>
                 </select>
             </div>
 
